6416207fdfd401b65238a176ae20b7c8f80202b8
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-02 11:25:04 -0300

    Functional refactoring.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- main.lua
+++ main.lua
@@ -1,13 +1,24 @@
+fun = require("fun")()
+
+function genTransformTo2D(width, height)
+  return function (n)
+    local ylogical = math.floor(n / width) 
+    local y = ylogical * height 
+    local x = n - ylogical * width 
+    return x, y
+  end
+end
+
 
 -- This could be a map of a sequence, to a 2d to 1d transformation and a newQuad call.
+-- Maybe use a generator for x, y coordinates?
 function generateQuads(width, height, rows, columns, tilesetWidth, tilesetHeight)
-  local quads = {}
-  for y=1,rows do
-    for x=1,columns do
-      local quad = love.graphics.newQuad((x-1)*width, (y-1)*height, width, height, tilesetWidth, tilesetHeight)
-      table.insert(quads, quad)
-    end
+  local totalQuads = rows * columns 
+  local xys = map(genTransformTo2D(width, height), range(0, totalQuads-1))
+  local createQuadAt = function (x, y)
+      return love.graphics.newQuad(x*width, y*width, width, height, tilesetWidth, tilesetHeight)
   end
+  local quads = totable(map(createQuadAt, xys))
   return quads
 end
 
@@ -36,6 +47,7 @@ end
 
 function drawMap(map, tileset, quads, tileWidth, tileHeight)
   -- Two dimensional iterator. Could be made by composing 2 maps and a draw clousure. 
+
   for i, row in ipairs(map) do
     for j, val in ipairs(row) do
       love.graphics.draw(Global.tileset, Global.quads[val], (i-1)*tileWidth, (j-1)*tileHeight)
