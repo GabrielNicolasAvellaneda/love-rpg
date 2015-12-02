08eda29a920e4a921be1a8fc76642bd0841f6774
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-02 00:06:42 -0300

    Map drawing using the tileset and quads
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- main.lua
+++ main.lua
@@ -1,12 +1,9 @@
 
-TileWidth = 32
-TileHeight = 32
-
 -- This could be a map of a sequence, to a 2d to 1d transformation and a newQuad call.
 function generateQuads(width, height, rows, columns, tilesetWidth, tilesetHeight)
   local quads = {}
-  for x=1,columns do
-    for y=1,rows do
+  for y=1,rows do
+    for x=1,columns do
       local quad = love.graphics.newQuad((x-1)*width, (y-1)*height, width, height, tilesetWidth, tilesetHeight)
       table.insert(quads, quad)
     end
@@ -19,12 +16,33 @@ function love.load()
   -- Generate a list of quads for a tileset.
   local tilesPerRow = 2
   local tilesRows = 2
-  local quads = generateQuads(TileWidth, TileHeight, tilesPerRow, tilesRows, tileset:getWidth(), tileset:getHeight())  
+  local tileWidth, tileHeight = 32, 32
+  local quads = generateQuads(tileWidth, tileHeight, tilesPerRow, tilesRows, tileset:getWidth(), tileset:getHeight())  
+
+  local map =
+  {
+    {1, 1, 1},
+    {1, 2, 1},
+    {1, 1, 1} 
+  }
+
   Global = {}
   Global.tileset = tileset
+  Global.tileWidth = tileWidth
+  Global.tileHeight = tileHeight
   Global.quads = quads
+  Global.map = map
+end
+
+function drawMap(map, tileset, quads, tileWidth, tileHeight)
+  -- Two dimensional iterator. Could be made by composing 2 maps and a draw clousure. 
+  for i, row in ipairs(map) do
+    for j, val in ipairs(row) do
+      love.graphics.draw(Global.tileset, Global.quads[val], (i-1)*tileWidth, (j-1)*tileHeight)
+    end
+  end
 end
 
 function love.draw()
-  love.graphics.draw(Global.tileset, Global.quads[1], 0, 0)
+  drawMap(Global.map, Global.tileset, Global.quads, Global.tileWidth, Global.tileHeight)
 end
