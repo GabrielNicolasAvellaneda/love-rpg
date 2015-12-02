b07901644784833d63b42e936158b83b4d4247f9
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-02 15:51:29 -0300

    Add translation for x coordinates for scrolling.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 fun = require("fun")()
 local lib = require("lib") 
 
+
+
+
 function genTransformTo2D(width, height)
   return function (n)
     local ylogical = math.floor(n / width) 
@@ -25,22 +28,16 @@ function loadMap(path)
   return f()
 end
 
-function love.load() 
-  local tileset = love.graphics.newImage("assets/tileset.png") 
-  -- Generate a list of quads for a tileset.
-  local tilesPerRow = 2
-  local tilesRows = 2
-  local tileWidth, tileHeight = 32, 32
-  local quads = generateQuads(tileWidth, tileHeight, tilesPerRow, tilesRows, tileset:getWidth(), tileset:getHeight())  
-
-  local map = loadMap("map_01.lua")
-  
-  Global = {}
-  Global.tileset = tileset
-  Global.tileWidth = tileWidth
-  Global.tileHeight = tileHeight
-  Global.quads = quads
-  Global.map = map
+function generateRandomMap(width, height)
+  local map = {}
+  for i=1,height do
+    local row = {}
+    for j=1,width do
+      table.insert(row, math.random(1,2))
+    end
+    table.insert(map, row)
+  end
+  return map
 end
 
 function eachCell(func, map)
@@ -62,6 +59,37 @@ function drawMap(map, tileset, quads, tileWidth, tileHeight)
     end, map)
  end
 
+-- Love callbacks
+
+function love.load() 
+  local tileset = love.graphics.newImage("assets/tileset.png") 
+  -- Generate a list of quads for a tileset.
+  local tilesPerRow = 2
+  local tilesRows = 2
+  local tileWidth, tileHeight = 32, 32
+  local quads = generateQuads(tileWidth, tileHeight, tilesPerRow, tilesRows, tileset:getWidth(), tileset:getHeight())  
+
+  --local map = loadMap("map_01.lua")
+  local map = generateRandomMap(64, 64)
+  
+  Global = {}
+  Global.tileset = tileset
+  Global.tileWidth = tileWidth
+  Global.tileHeight = tileHeight
+  Global.quads = quads
+  Global.map = map
+  Global.offset = 0
+end
+
+function love.keypressed(key, isrepeat)
+
+end
+
+function love.update()
+  Global.offset = Global.offset + 4 
+end
+
 function love.draw()
+  love.graphics.translate(Global.offset, 0)
   drawMap(Global.map, Global.tileset, Global.quads, Global.tileWidth, Global.tileHeight)
 end
