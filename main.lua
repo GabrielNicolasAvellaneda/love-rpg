32aeca6dca794a69d0df154d5a2af8210a805333
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-01 23:28:32 -0300

    Use a quad to select a particular tile from tileset.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
 
+
+
 function love.load() 
-  Tileset = love.graphics.newImage("assets/tileset.png") 
+  local tileset = love.graphics.newImage("assets/tileset.png") 
+  local quad = love.graphics.newQuad(0, 0, 32, 32, tileset:getWidth(), tileset:getHeight())  
+
+  Global = {}
+  Global.tileset = tileset
+  Global.quads = { quad }
 end
 
 function love.draw()
-  love.graphics.draw(Tileset, 0, 0)
+  love.graphics.draw(Global.tileset, Global.quads[1], 0, 0)
 end
